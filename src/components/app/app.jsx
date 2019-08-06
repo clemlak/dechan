386ac37883e760ad5e0a4b6d8f4ee695e13eb80d
Clement Lakhal <clementlakhal@gmail.com>
2019-08-06 03:00:39 +0200

    Update

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,13 +3,58 @@ import React, {
   useContext,
   useState,
 } from 'react';
+import styled from 'styled-components';
 import IPFS from 'ipfs';
 import OrbitDB from 'orbit-db';
+import {
+  Flex,
+  Box,
+} from '@rebass/grid';
+
+const Card = styled.div`
+  width: 250px;
+  min-height: 100px;
+  background-color: #f5f5f5;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  padding: 10px;
+`;
+
+const Label = styled.label`
+  display: block;
+`;
+
+const Input = styled.input`
+  display: block;
+`;
+
+const CreatedAt = styled.span`
+  font-family: 'Rubik', sans-serif;
+  color: #c1c1c1;
+  font-size: 12px;
+`;
+
+const Tag = styled.span`
+  font-family: 'Rubik', sans-serif;
+  color: #c1c1c1;
+  font-size: 12px;
+`;
+
+const Message = styled.span`
+  font-family: 'Rubik', sans-serif;
+  color: #010101;
+  font-size: 14px;
+`;
 
 function App() {
   const [db, setDb] = useState();
-  const [msg, setMsg] = useState('');
   const [chat, setChat] = useState([]);
+
+  const [msg, setMsg] = useState('');
+  const [pic, setPic] = useState('');
+  const [link, setLink] = useState('');
+  const [tag, setTag] = useState('');
+
 
   useEffect(() => {
     function init() {
@@ -33,7 +78,7 @@ function App() {
       ipfs.on('ready', async () => {
         const orbitdb = await OrbitDB.createInstance(ipfs);
 
-        /* const loadedDb = await orbitdb.create('posts', 'eventlog', {
+        /* const loadedDb = await orbitdb.create('postables', 'eventlog', {
           accessController: {
             write: [
               '*',
@@ -41,13 +86,7 @@ function App() {
           },
         }); */
 
-        const loadedDb = await orbitdb.open('/orbitdb/zdpuAnkXDMG5MxLSAXjh9dKJT7KMaT83Po5CChgVAbnik5Lbc/posts');
-
-        /* const loadedDb = await orbitdb.create('dechan', 'eventlog', {
-          accessController: {
-            write: ['*'],
-          },
-        }); */
+        const loadedDb = await orbitdb.open('/orbitdb/zdpuB3WiH3zDw1izSwAZBaoTnC6LFvdtymWzVxbPCTY99kMP9/postables');
 
         console.log(loadedDb.address);
 
@@ -104,7 +143,15 @@ function App() {
   }, []);
 
   async function send() {
-    const hash = await db.add(msg);
+    const msgToAdd = {
+      pic,
+      link,
+      message: msg,
+      createdAt: Date.now(),
+      tag,
+    };
+
+    const hash = await db.add(msgToAdd);
     console.log(hash);
     setMsg('');
   }
@@ -114,9 +161,37 @@ function App() {
 
     for (let i = 0; i < chat.length; i += 1) {
       messages.push(
-        <p key={i}>
-          {chat[i]}
-        </p>,
+        <Card key={chat[i].createdAt}>
+          <Flex>
+            <Box width={1 / 2}>
+              <CreatedAt>
+                {chat[i].createdAt}
+              </CreatedAt>
+            </Box>
+            <Box width={1 / 2}>
+              <Tag>
+                {chat[i].tag}
+              </Tag>
+            </Box>
+          </Flex>
+          {chat[i].pic !== '' && (
+            <Box>
+              <img src={chat[i].pic} alt="img" width={150} />
+            </Box>
+          )}
+          <Box>
+            <Message>
+              {chat[i].message}
+            </Message>
+          </Box>
+          {chat[i].link !== '' && (
+            <Box>
+              <a href={chat[i].link} target="_blank" rel="noopener noreferrer">
+                Visit link
+              </a>
+            </Box>
+          )}
+        </Card>,
       );
     }
 
@@ -126,12 +201,46 @@ function App() {
   return (
     <>
       <h1>Dechan</h1>
-      <input
-        type="text"
-        placeholder="Message"
-        value={msg}
-        onChange={e => setMsg(e.target.value)}
-      />
+      <Label htmlFor="message">
+        Message
+        <Input
+          name="message"
+          type="text"
+          placeholder="Message"
+          value={msg}
+          onChange={e => setMsg(e.target.value)}
+        />
+      </Label>
+      <Label htmlFor="pic">
+        Picture
+        <Input
+          name="pic"
+          type="text"
+          placeholder="http://imgur.com/..."
+          value={pic}
+          onChange={e => setPic(e.target.value)}
+        />
+      </Label>
+      <Label htmlFor="link">
+        Link
+        <Input
+          name="link"
+          type="text"
+          placeholder="http://wikipedia.org/..."
+          value={link}
+          onChange={e => setLink(e.target.value)}
+        />
+      </Label>
+      <Label htmlFor="tag">
+        Tag
+        <Input
+          name="tag"
+          type="text"
+          placeholder="Funny"
+          value={tag}
+          onChange={e => setTag(e.target.value)}
+        />
+      </Label>
       <button type="button" onClick={() => send()}>
         Send
       </button>
